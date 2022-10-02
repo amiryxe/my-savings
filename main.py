aa41ca88b34437fc8072443911d39c9486cd86b5
Amir Salehi <nik.amirsa@gmail.com>
2022-10-02 08:52:53 +0330

    add messages in command and get data as a while

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 savings = {}
 
-inputName = input("Enter saving name: ")
-inputValue = int(input("Enter saving value: "))
+print("\n")
+print("Hi! Enter your saving list, I will calculate total for you")
+print(
+    "As long as the word 'end' is not entered for name, receiving information continues"
+)
+print("\n")
+
+inputName = ""
+
+while inputName != 'end':
+    inputName = input("+   Saving name: ")
+    inputValue = int(input("+   Saving value: "))
+    print("\n")
 
 print(inputName, inputValue)
