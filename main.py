4beac0e1ef1853877a12b5fb2481410975ab212c
Amir Salehi <nik.amirsa@gmail.com>
2022-10-02 17:45:15 +0330

    calculate saving result

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from itertools import count
+from prettytable import PrettyTable
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+
 savings = {}
 
 print("\n")
@@ -7,11 +13,37 @@ print(
 )
 print("\n")
 
-inputName = ""
+newStatus = ""
 
-while inputName != 'end':
+my_table = PrettyTable()
+my_table.field_names = ["No.", "Name", "Value"]
+
+count = 0
+
+while newStatus != 'n':
     inputName = input("+   Saving name: ")
     inputValue = int(input("+   Saving value: "))
+
+    count += 1
+
+    my_table.add_row([count, inputName, inputValue])
+
+    newStatus = input("\nEnter New Item? (y/n): ")
+    savings[inputName] = inputValue
     print("\n")
 
-print(inputName, inputValue)
+values = savings.values()
+
+
+def intWithCommas(x):
+    if x < 0:
+        return '-' + intWithCommas(-x)
+    result = ''
+    while x >= 1000:
+        x, r = divmod(x, 1000)
+        result = ",%03d%s" % (r, result)
+    return "%d%s" % (x, result)
+
+
+print(my_table)
+print("SAVINGS RESULT: ", intWithCommas(sum(values)))
